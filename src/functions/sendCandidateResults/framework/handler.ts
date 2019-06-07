57cfed0c8dfd28f13b891f82e21d0f179a379efd
szabi.keresztes <szabi.keresztes@bjss.com>
2019-06-07 11:42:49 +0100

    Configuring initial env variables

--- src/functions/sendCandidateResults/framework/handler.ts
+++ src/functions/sendCandidateResults/framework/handler.ts
@@ -1,2 +1,18 @@
 export async function handler() {
+
+  const isLocal = process.env.IS_LOCAL || false;
+  const useNotify = process.env.USE_NOTIFY || false;
+
+  if (isLocal) {
+    console.log('Use stub notify');
+    return {};
+  }
+
+  if (useNotify) {
+    console.log('Use Gov notify');
+    return {};
+  }
+
+  console.log('Log to CloudWatch');
+
 }
