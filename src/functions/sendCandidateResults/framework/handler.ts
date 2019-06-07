391f5a5e48dd04f280e361ad56e9fce4ba55fd26
Tom Billington <t.billington@kainos.com>
2019-06-07 14:47:40 +0100

    Bottleneck Config

--- src/functions/sendCandidateResults/framework/handler.ts
+++ src/functions/sendCandidateResults/framework/handler.ts
@@ -1,18 +1,41 @@
+import bottleneck from 'bottleneck';
+
 export async function handler() {
+    // TODO - This needs to be gotten from the post
+  const testResults = [...Array(50).keys()];
+
+  const limiter = new bottleneck({
+    maxConcurrent: null,                 // No limit on concurrent requests
+    minTime: 0,                          // No time waited between each request
+    highWater: 250,                      // Maximum of 250 requests in the queue (max batch size)
+    strategy: bottleneck.strategy.BLOCK, // Ignore any additions to the queue when we reach the max batch size
+    reservoir: 25,                       // Amount of jobs the queue can perform at the start of the queue
+    reservoirRefreshInterval: 1000,      // How often to add new jobs to the queue (every second)
+    reservoirRefreshAmount: 25,          // How many jobs to add to the queue each refresh
+
+  });
+
+  testResults.forEach((testResult) => {
+    limiter
+    .schedule(() => sendNotifyRequest(testResult))
+    .then(success => console.log('success')) // TODO - Tell Database test result has been sent
+    .catch(error => console.log('error')); // TODO - Tell Database test result has not been sent
+  });
+}
+
+function sendNotifyRequest(testResult: any): Promise<any>  {
 
   const isLocal = process.env.IS_LOCAL || false;
   const useNotify = process.env.USE_NOTIFY || false;
 
   if (isLocal) {
     console.log('Use stub notify');
-    return {};
   }
 
   if (useNotify) {
     console.log('Use Gov notify');
-    return {};
   }
 
   console.log('Log to CloudWatch');
-
+  return new Promise(resolve => resolve(1));
 }
