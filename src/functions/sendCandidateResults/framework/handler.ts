4413ec6578a0431ea221710b479c562a1730fa25
Ammar Haider <ammar.haider@bjss.com>
2019-06-13 15:37:04 +0100

    calls next upload batch

--- src/functions/sendCandidateResults/framework/handler.ts
+++ src/functions/sendCandidateResults/framework/handler.ts
@@ -5,16 +5,18 @@ import { IRequestScheduler } from './request-scheduler';
 import { INextUploadBatch } from '../domain/next-upload-batch.interface';
 
 export async function handler() {
-
+  // TODO -  Use Real Service + get batch size from config
   let testResults: StandardCarTestCATBSchema[];
   const nextUploadBatch = container.get<INextUploadBatch>(TYPES.INextUploadBatch);
 
   try {
+    console.log('### handler ####');
     testResults = await nextUploadBatch.get();
+
     const requestScheduler: IRequestScheduler = container.get<IRequestScheduler>(TYPES.IRequestScheduler);
     requestScheduler.scheduleRequests(testResults);
   } catch (err) {
-    console.log(`### err:  ${err}`);
-    throw(err);
+    console.log('### Error fetching next upload batch ####');
+    console.log(err);
   }
 }
