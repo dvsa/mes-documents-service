013c5796690f88420557e7ffc029825414e8ab8e
Tom Billington <t.billington@kainos.com>
2019-06-14 09:22:47 +0100

    Updated Mock Object so it matches what notify gives us

--- src/functions/sendCandidateResults/application/stub/notify-client-stub-failure-400.ts
+++ src/functions/sendCandidateResults/application/stub/notify-client-stub-failure-400.ts
@@ -15,11 +15,13 @@ export class NotifyClientStubFailure400 implements INotifyClient {
     return Promise.reject({
       error: {
         status_code: 400,
-        errors: {
-          error: 'BadRequestError',
-          // tslint:disable-next-line
-          message: 'Can not send to this recipient when service is in trial mode - see www.notifications.service.gov.uk/trial-mode',
-        },
+        errors: [
+          {
+            error: 'BadRequestError',
+            // tslint:disable-next-line
+            message: 'Can not send to this recipient when service is in trial mode - see www.notifications.service.gov.uk/trial-mode',
+          },
+        ],
       },
     });
   }
@@ -33,11 +35,13 @@ export class NotifyClientStubFailure400 implements INotifyClient {
     return Promise.reject({
       error: {
         status_code: 400,
-        errors: {
-          error: 'BadRequestError',
-          // tslint:disable-next-line
-          message: 'Cannot send letters when service is in trial mode - see https://www.notifications.service.gov.uk/trial-mode',
-        },
+        errors: [
+          {
+            error: 'BadRequestError',
+            // tslint:disable-next-line
+            message: 'Cannot send letters when service is in trial mode - see https://www.notifications.service.gov.uk/trial-mode',
+          },
+        ],
       },
     });
   }
