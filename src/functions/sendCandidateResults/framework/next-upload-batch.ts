3f36b5731b3319fe912e9f350a92ba6d9e1f3a23
ammarhaiderbjss <33695049+ammarhaiderbjss@users.noreply.github.com>
2019-06-17 14:07:37 +0100

    Mes 2604 call next upload batch (#21)
    * calls next upload batch
    
    * throws on error
    
    * logs
    
    * tidy up
    
    * processing upload batch
    
    * removes zlib from deps
    
    * fixes TS errors

--- src/functions/sendCandidateResults/framework/next-upload-batch.ts
+++ src/functions/sendCandidateResults/framework/next-upload-batch.ts
@@ -1,8 +1,12 @@
-import { INextUploadBatch } from '../domain/next-upload-batch.interface';
 import { injectable, inject } from 'inversify';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { StandardCarTestCATBSchema } from '@dvsa/mes-test-schema/categories/B';
+import * as zlib from 'zlib';
+
+import { INextUploadBatch } from '../domain/next-upload-batch.interface';
 import { TYPES } from './di/types';
 import { IConfigAdapter } from './adapter/config/config-adapter.interface';
+import { TestResultError } from './errors/TestResultError';
 
 @injectable()
 export class NextUploadBatch implements INextUploadBatch {
@@ -20,6 +24,27 @@ export class NextUploadBatch implements INextUploadBatch {
     const { resultsBaseApiUrl } = this.configAdapter;
     return axios.get(
       `${resultsBaseApiUrl}/test-results/upload?interface=${this.interface}&batch_size=${this.batchSize}`,
-    );
+    ).then((response: AxiosResponse): StandardCarTestCATBSchema[] => {
+      const parseResult = response.data;
+      const resultList: StandardCarTestCATBSchema[] = [];
+      parseResult.forEach((element: string) => {
+        let uncompressedResult: string = '';
+        let test: StandardCarTestCATBSchema;
+
+        try {
+          uncompressedResult = zlib.gunzipSync(new Buffer(element, 'base64')).toString();
+        } catch (e) {
+          return new TestResultError('failed decompressing test result');
+        }
+
+        try {
+          test = JSON.parse(uncompressedResult);
+          resultList.push(test);
+        } catch (e) {
+          return new TestResultError('failed parsing test result');
+        }
+      });
+      return resultList;
+    });
   }
 }
