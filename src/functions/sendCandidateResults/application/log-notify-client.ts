0dfaf88f02581b8edf3e42f0bfa344eec1e664eb
szabi.keresztes <szabi.keresztes@bjss.com>
2019-06-18 10:09:16 +0100

    Adding meaningful logs

--- src/functions/sendCandidateResults/application/log-notify-client.ts
+++ src/functions/sendCandidateResults/application/log-notify-client.ts
@@ -4,12 +4,14 @@ import { INotifyClient } from '../domain/notify-client.interface';
 @injectable()
 export class LogNotifyClient implements INotifyClient {
   sendEmail(templateId: string, emailAddress: string, options: any): Promise<any> {
-    console.log('log this to CloudWatch');
+    console.log(`email would be sent to ${emailAddress},
+      first name: ${options.personalisation.firstName}, on date: ${options.personalisation.date}`);
     return Promise.resolve({});
   }
 
   sendLetter(templateId: string, options: any): Promise<any> {
-    console.log('log this to CloudWatch');
+    console.log(`letter would be sent for ${options.personalisation.firstName},
+      to postcode: ${options.personalisation.postcode}, on date: ${options.personalisation.date}`);
     return Promise.resolve({});
   }
 
