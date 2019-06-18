3383c9cac24d3e0b511bd8d152c215dd9a835327
szabi.keresztes <szabi.keresztes@bjss.com>
2019-06-18 10:25:07 +0100

    logging out the entire personalisation property

--- src/functions/sendCandidateResults/application/log-notify-client.ts
+++ src/functions/sendCandidateResults/application/log-notify-client.ts
@@ -4,14 +4,12 @@ import { INotifyClient } from '../domain/notify-client.interface';
 @injectable()
 export class LogNotifyClient implements INotifyClient {
   sendEmail(templateId: string, emailAddress: string, options: any): Promise<any> {
-    console.log(`email would be sent to ${emailAddress},
-      first name: ${options.personalisation.firstName}, on date: ${options.personalisation.date}`);
+    console.log(`email would be sent to ${emailAddress}, personalisation: ${JSON.stringify(options.personalisation)}`);
     return Promise.resolve({});
   }
 
   sendLetter(templateId: string, options: any): Promise<any> {
-    console.log(`letter would be sent for ${options.personalisation.firstName},
-      to postcode: ${options.personalisation.postcode}, on date: ${options.personalisation.date}`);
+    console.log(`letter would be sent, personalisation: ${JSON.stringify(options.personalisation)}`);
     return Promise.resolve({});
   }
 
