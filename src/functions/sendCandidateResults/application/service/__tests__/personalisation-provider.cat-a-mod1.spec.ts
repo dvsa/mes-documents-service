da69b1540b6af8ca7173eb0cdea949f80727ebce
matthew <m.bell2412@hotmail.com>
2020-07-22 13:34:41 +0100

    update competency labels to be in correct casing and update tests accordingly

--- src/functions/sendCandidateResults/application/service/__tests__/personalisation-provider.cat-a-mod1.spec.ts
+++ src/functions/sendCandidateResults/application/service/__tests__/personalisation-provider.cat-a-mod1.spec.ts
@@ -3,6 +3,7 @@ import { IFaultProvider, FaultProvider } from '../fault-provider';
 import { ICustomPropertyProvider, CustomPropertyProvider } from '../custom-property-provider';
 import { completedCatAMod1Test } from '../../../framework/__mocks__/test-data.cat-a-mod1.mock';
 import { BooleanText, CatAMod1Personalisation } from '../../../domain/personalisation.model';
+import { modifiedEnglishCompetencyLabels } from '../../../domain/competencies';
 
 describe('personalisation-provider-cat-a-mod1', () => {
 
@@ -27,11 +28,12 @@ describe('personalisation-provider-cat-a-mod1', () => {
 
       const result = personalisationProvider.getEmailPersonalisation(completedCatAMod1Test) as CatAMod1Personalisation;
 
-      expect(result.drivingFaults).toEqual(['Move Away - Safety, 3', 'Move Away - Control, 1']);
-      expect(result.dangerousFaults).toContain('Move Away - Safety');
-      expect(result.dangerousFaults).toContain('Move Away - Control');
-      expect(result.seriousFaults).toContain('Move Away - Safety');
-      expect(result.seriousFaults).toContain('Move Away - Control');
+      expect(result.drivingFaults).toContain(`${modifiedEnglishCompetencyLabels.moveOffSafety}, 3`);
+      expect(result.drivingFaults).toContain(`${modifiedEnglishCompetencyLabels.moveOffControl}, 1`);
+      expect(result.dangerousFaults).toContain(`${modifiedEnglishCompetencyLabels.moveOffSafety}`);
+      expect(result.dangerousFaults).toContain(`${modifiedEnglishCompetencyLabels.moveOffControl}`);
+      expect(result.seriousFaults).toContain(`${modifiedEnglishCompetencyLabels.moveOffControl}`);
+      expect(result.seriousFaults).toContain(`${modifiedEnglishCompetencyLabels.moveOffControl}`);
 
       expect(result.showEmergencyStop).toEqual(BooleanText.YES);
       expect(result.showEmergencyFirstAttempt).toEqual(BooleanText.YES);
