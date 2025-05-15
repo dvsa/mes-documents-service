b9bb79f06279474343b6f5ed591e085dfb63e5bb
Andrew Setterfield <48550267+andrewsetterfield@users.noreply.github.com>
2025-05-15 09:46:42 +0100

    feat: link up template for padi (#311)

--- src/functions/sendCandidateResults/application/service/send-notification.ts
+++ src/functions/sendCandidateResults/application/service/send-notification.ts
@@ -1,15 +1,16 @@
 /* eslint-disable max-len */
-import {DocumentsServiceError} from '../../domain/errors/documents-service-error';
-import {Address, PersonalisationDetails} from '../../domain/personalisation.model';
-import {INotifyClient} from '../../domain/notify-client.interface';
-import {get, isNil, omitBy} from 'lodash';
-import {AxiosError} from 'axios';
+import { DocumentsServiceError } from '../../domain/errors/documents-service-error';
+import { Address, PersonalisationDetails } from '../../domain/personalisation.model';
+import { INotifyClient } from '../../domain/notify-client.interface';
+import { get, isNil, omitBy } from 'lodash';
+import { AxiosError } from 'axios';
 import * as Handlebars from 'handlebars';
-import {subjectMapper, templateMapper} from './template-selector';
-import {Language} from '../../domain/conducted-language';
-import {TestOutcome} from '../../domain/test-outcome';
-import {CommunicationMethod} from '@dvsa/mes-test-schema/categories/common';
-import {Correspondence} from '../../domain/template-id.model';
+import { subjectMapper, templateMapper } from './template-selector';
+import { Language } from '../../domain/conducted-language';
+import { TestOutcome } from '../../domain/test-outcome';
+import { CommunicationMethod } from '@dvsa/mes-test-schema/categories/common';
+import { Correspondence } from '../../domain/template-id.model';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
 export interface Personalisation extends Partial<Address>{
   renderedSubject: string | undefined;
@@ -26,6 +27,7 @@ export async function sendNotification(
   client: INotifyClient,
   conductedLanguage: Language,
   testOutcome: TestOutcome,
+  padi: boolean = false,
 ): Promise<any> {
 
   let personalisation: Personalisation;
@@ -34,14 +36,14 @@ export async function sendNotification(
   let address: Address;
 
   try {
-    renderedSubject = getRenderedSubject(notificationPersonalisation, conductedLanguage);
+    renderedSubject = getRenderedSubject(notificationPersonalisation, conductedLanguage, padi);
   } catch (subjectError) {
     console.error('Error preparing rendered subject', subjectError);
     throw new Error('Error preparing rendered content');
   }
 
   try {
-    renderedText = getRenderedText(testOutcome, notificationPersonalisation, conductedLanguage);
+    renderedText = getRenderedText(testOutcome, notificationPersonalisation, conductedLanguage, padi);
   } catch (textError) {
     console.error('Error preparing rendered text', textError);
     throw new Error('Error preparing rendered content');
@@ -92,12 +94,15 @@ export async function sendNotification(
  * Compile subject from the appropriate category and personalisation
  * @param notificationPersonalisation
  * @param conductedLanguage
+ * @param padi
  */
 export function getRenderedSubject(
   notificationPersonalisation: PersonalisationDetails,
   conductedLanguage: Language,
+  padi: boolean = false,
 ): string | undefined {
   try {
+    if (padi) return notificationPersonalisation.category === TestCategory.SC ? 'ADI SC – 3rd Attempt Fail' : 'ADI Part 3 - 3rd Attempt Fail';
     const compileSubject = Handlebars.compile(
       subjectMapper(notificationPersonalisation.category, conductedLanguage)
     );
@@ -118,11 +123,12 @@ export function getRenderedText(
   testOutcome: TestOutcome,
   notificationPersonalisation: PersonalisationDetails,
   conductedLanguage: Language,
+  padi: boolean = false,
 ): string | undefined {
   try {
     const compileTemplate = Handlebars.compile(
       // eslint-disable-next-line max-len
-      templateMapper(testOutcome, notificationPersonalisation.category, conductedLanguage, notificationPersonalisation.previousAttempts)
+      templateMapper(testOutcome, notificationPersonalisation.category, conductedLanguage, padi, notificationPersonalisation.previousAttempts)
     );
     return compileTemplate(notificationPersonalisation);
   } catch (error) {
